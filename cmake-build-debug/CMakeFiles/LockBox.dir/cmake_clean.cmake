05ee7fc511333b856bed8b14ac3483c98c4eee5c
qqbeach <danildzekelev@gmail.com>
2023-04-18 15:51:37 +0300

    Init main.cpp.

---
+++ cmake-build-debug/CMakeFiles/LockBox.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LockBox.dir/main.cpp.obj"
+  "LockBox.exe"
+  "LockBox.exe.manifest"
+  "LockBox.pdb"
+  "libLockBox.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LockBox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
