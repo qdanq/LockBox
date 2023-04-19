ed48e08214b4018e6ab5f1a9bbfbb8eaa9e029fb
qqbeach <danildzekelev@gmail.com>
2023-04-19 14:57:37 +0300

    Add path input and create pass.txt in path.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
+#include "fstream"
 
 using namespace std;
 
 int main() {
-    string password, prompt_password;
-    cout << "Create a password" << endl;
+    string password, prompt_password, path;
+    cout << "Path to directory to protect: ";
+    cin >> path;
+    path += "\\pass.txt";
+
+    cout << path;
+
+
+    ofstream fout;
+    fout.open(path);
+
+
+    /*cout << "\nCreate a password: ";
     cin >> password;
-    cout << "Enter password" << endl;
+    cout << "\nEnter password: ";
     cin >> prompt_password;
     if (prompt_password == password) {
         cout << "+";
-    }
+     } */
 }
