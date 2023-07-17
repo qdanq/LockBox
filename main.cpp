82c9b0a3880a09070b91952f64f919b09834fb9c
qqbeach <vapraobpukvr@gmail.com>
2023-07-17 15:06:47 +0300

    Add rename folder process.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+// TODO: для начала реализуй блокировку, потом с базами данных и как взаимодействовать с уже заблокироваными папками
+// и запоминать их, а потом уже разблокировка.
 // TODO: ОСНОВНОЙ ПРИНЦИП РАБОТЫ. Вводим путь, проверяем существует ли он, не запаролен ли он,
 //  если запаролен, то проверка на пароль. Если нет то используем блок механизм, накидываем атрибут,
 //  сохраняем название папки, подключи БД.
@@ -20,13 +23,13 @@ public:
 
 
 int main() {
-    Folder tempFolder{"D:\\dsa", "dsa", "123", true};
+    Folder tempFolder{};
 
-    string path, folder, password;
+    string folder, password, path; // поменяй местами с класом, а то некрасиво что глобалки короткие а локалки класса длинные
 
-    /* cout << "Input path to folder:" << endl;
-     cin >> path;
-     folder = path.substr(path.find_last_of('\\') + 1, -1);  '+ 1' for get rid of last '\' before folder name*/
+    cout << "Input folder path:" << endl;
+    cin >> path;
+    folder = path.substr(path.find_last_of('\\') + 1, -1); // '+ 1' for get rid of last '\' before folder name
 
 
     if (tempFolder.exists) {
@@ -42,6 +45,12 @@ int main() {
         }
     }
 
+    string newPath = path + "123";
+
+    const char * charPath = path.c_str();
+    const char * charNewPath = newPath.c_str();
+
+    rename(charPath, charNewPath);
 
     return 0;
 }
